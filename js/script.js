ef4a91e390d55b39d17bf5051a120277164ea45e
johannranudd <33978097+johannranudd@users.noreply.github.com>
2021-08-23 14:56:39 +0200

    zdfgnhm

--- js/script.js
+++ js/script.js
@@ -142,72 +142,164 @@
 // john.fullName();
 
 // !prototype
+// !test
+// // const buy = document.querySelector(".buy");
+// // const buy30 = document.querySelector(".buy30");
+// // const sell = document.querySelector(".sell");
+// // const sell30 = document.querySelector(".sell30");
+// const leftOverH1 = document.querySelector(".left-over");
+// const myStore = document.querySelector(".my-store");
 
-// const buy = document.querySelector(".buy");
-// const buy30 = document.querySelector(".buy30");
-// const sell = document.querySelector(".sell");
-// const sell30 = document.querySelector(".sell30");
-const leftOverH1 = document.querySelector(".left-over");
-const myStore = document.querySelector(".my-store");
+// function Account(name, initialBalance, fee, leftOver) {
+//   this.name = name;
+//   this.fee = fee;
+//   this.leftOver = leftOver;
+//   this.initialBalance = initialBalance;
+//   this.currentBalance = function (
+//     name,
+//     initialBalance,
+//     buyOrSell,
+//     fee,
+//     leftOver
+//   ) {
+//     console.log(`${name} has ${initialBalance}, ${buyOrSell}
+//         ${fee} and has ${leftOver} money left`);
+//   };
+// }
 
-function Account(name, initialBalance, fee, leftOver) {
-  this.name = name;
-  this.fee = fee;
-  this.leftOver = leftOver;
-  this.initialBalance = initialBalance;
-  this.currentBalance = function (
-    name,
-    initialBalance,
-    buyOrSell,
-    fee,
-    leftOver
-  ) {
-    console.log(`${name} has ${initialBalance}, ${buyOrSell} 
-        ${fee} and has ${leftOver} money left`);
-  };
+// const susy = new Account();
+// // susy.currentBalance('susy', initial, fee, leftOver);
+
+// let withdrawAmount = 1000;
+// let initial = 1000;
+// let fee = 100;
+// let cheapThing = 30;
+// let leftOver = initial;
+
+// // buy
+// function buyFn(item) {
+//   leftOver = initial - item;
+//   susy.currentBalance("susy", initial, "bought for", item, leftOver);
+//   initial = initial - item;
+// }
+// // sell
+// function sellFn(item) {
+//   leftOver = initial + item;
+//   susy.currentBalance("susy", initial, "sold for", item, leftOver);
+//   initial = initial + item;
+// }
+
+// myStore.addEventListener("click", function (e) {
+//   const id = e.target.dataset.id;
+//   if (id === "buy") {
+//     buyFn(fee);
+//     leftOverH1.textContent = `you have ${leftOver} money left`;
+//   } else if (id === "buy30") {
+//     buyFn(cheapThing);
+//     leftOverH1.textContent = `you have ${leftOver} money left`;
+//   } else if (id === "sell") {
+//     sellFn(fee);
+//     leftOverH1.textContent = `you have ${leftOver} money left`;
+//   } else if (id === "sell30") {
+//     sellFn(cheapThing);
+//     leftOverH1.textContent = `you have ${leftOver} money left`;
+//   } else if (id === "withdraw") {
+//     initial += withdrawAmount;
+//     leftOverH1.textContent = `you have withdrawn ${withdrawAmount} money`;
+//   }
+//   if (leftOver <= 0) {
+//     leftOverH1.textContent = `you are out of money, withdraw more to continue buying`;
+//   }
+// });
+
+// !end test
+
+// !prototype
+
+// function Account(name, initialBalance) {
+//   this.name = name;
+//   this.balance = initialBalance;
+//   this.deposit = function(amount) {
+//     this.balance += amount;
+//     console.log(`Hello ${this.name}, your initial balance was ${initialBalance}
+//     and is now ${this.balance} after depositing ${amount}`);
+//   }
+// }
+
+// const john = new Account('John', 1000);
+// const bob = new Account('Bob', 500);
+// john.deposit(100);
+// bob.deposit(300);
+
+
+// ****
+// function Account(name, initialBalance) {
+//   this.name = name;
+//   this.initialBalance = initialBalance;
+//   this.balance = initialBalance;
+// }
+
+// Account.prototype.bank = 'CHASE';
+// Account.prototype.deposit = function (amount) {
+//   this.balance += amount;
+//   console.log(`Hello ${this.name}, you initial balance was ${this.initialBalance}
+//   and is now ${this.balance} after depositing ${amount}`);
+// };
+
+// const john = new Account("John", 1000);
+// const bob = new Account("Bob", 500);
+// john.deposit(100);
+// bob.deposit(400);
+// ****
+
+// *****
+// const nameInput = document.querySelector('#name');
+// const amountInput = document.querySelector('#amount');
+// const depositBtn = document.querySelector('.deposit-btn');
+// const result = document.querySelector('.result');
+
+
+// function checkAccount(name, initialBalance) {
+//   this.name = name;
+//   this.initialBalance = initialBalance;
+//   this.balance = initialBalance;
+//   this.checking = function(amount) {
+//     this.balance = amount;
+//     return `Hello ${this.name}, you initial balance was ${this.initialBalance}
+//     and is now ${this.balance} after depositing ${amount}`;
+//   }
+//   // this.initialBalance = this.balance;
+//   // return this.initialBalance;
+// }
+
+// depositBtn.addEventListener("click", function() {
+//   const person = new checkAccount(nameInput.value, 0)
+//   result.innerHTML = person.checking(amountInput.value);
+// })
+// ****
+
+const nameInput = document.querySelector('#name');
+const amountInput = document.querySelector('#amount');
+const depositBtn = document.querySelector('.deposit-btn');
+const result = document.querySelector('.result');
+
+let myBalance = 0;
+
+function balanceFn() {
+  const name = nameInput.value;
+  let deposit = parseInt(amountInput.value);
+  let newBalance = deposit + myBalance;
+  const resultText = `name: ${name}
+  initial balance: ${myBalance}
+  deposit: ${deposit}
+  new balance: ${newBalance}`;
+  
+  // deposit = parseInt(amountInput.value);
+
+  result.textContent = resultText;
+  myBalance = newBalance;
 }
 
-const susy = new Account();
-// susy.currentBalance('susy', initial, fee, leftOver);
 
-let withdrawAmount = 1000;
-let initial = 1000;
-let fee = 100;
-let cheapThing = 30;
-let leftOver = initial;
+depositBtn.addEventListener("click", balanceFn)
 
-// buy
-function buyFn(item) {
-  leftOver = initial - item;
-  susy.currentBalance("susy", initial, "bought for", item, leftOver);
-  initial = initial - item;
-}
-// sell
-function sellFn(item) {
-  leftOver = initial + item;
-  susy.currentBalance("susy", initial, "sold for", item, leftOver);
-  initial = initial + item;
-}
-
-myStore.addEventListener("click", function (e) {
-  const id = e.target.dataset.id;
-  if (id === "buy") {
-    buyFn(fee);
-    leftOverH1.textContent = `you have ${leftOver} money left`;
-  } else if (id === "buy30") {
-    buyFn(cheapThing);
-    leftOverH1.textContent = `you have ${leftOver} money left`;
-  } else if (id === "sell") {
-    sellFn(fee);
-    leftOverH1.textContent = `you have ${leftOver} money left`;
-  } else if (id === "sell30") {
-    sellFn(cheapThing);
-    leftOverH1.textContent = `you have ${leftOver} money left`;
-  } else if (id === "withdraw") {
-    initial += withdrawAmount;
-    leftOverH1.textContent = `you have withdrawn ${withdrawAmount} money`;
-  }
-  if (leftOver <= 0) {
-    leftOverH1.textContent = `you are out of money, withdraw more to continue buying`;
-  }
-});
