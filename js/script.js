0109aaf0a98297434ddc285e4a0f86d3da956cd1
johannranudd <33978097+johannranudd@users.noreply.github.com>
2021-08-26 22:19:04 +0200

    sdfdg

--- js/script.js
+++ js/script.js
@@ -35,7 +35,7 @@
 //   deposit(amount) {
 //     this.balance += amount;
 //     return `hello ${this.name} your balance was ${myBalance}
-//     and is ${this.balance} 
+//     and is ${this.balance}
 //     after depositing ${amount}`;
 //   }
 // }
@@ -54,7 +54,6 @@
 
 // !END TEST
 
-
 // const john = {
 //   name: 'john',
 //   age: 24,
@@ -64,36 +63,118 @@
 //   age: 25,
 // }
 
-const people = {
-  john: {
-    name: 'john',
-    age: 34,
-    ocupation: 'developer',
-  },
-  susan: {
-    name: 'susan',
-    age: 24,
-    ocupation: 'designer',
-  },
-  bob: {
-    name: 'bob',
-    age: 22,
-    ocupation: 'salesman',
-  },
-}
-
-// function greetFn(item) {
-//   const newPeople = 
-// }
-// function greet() {
-//   // console.log(this);
-//     console.log(`hello ${this.name} you are ${this.age} 
-//     years old ocupation is ${this.ocupation}`);
-  
+// const people = {
+//   john: {
+//     name: 'john',
+//     age: 34,
+//     ocupation: 'developer',
+//   },
+//   susan: {
+//     name: 'susan',
+//     age: 24,
+//     ocupation: 'designer',
+//   },
+//   bob: {
+//     name: 'bob',
+//     age: 22,
+//     ocupation: 'salesman',
+//   },
 // }
 
-// // console.log(people.john.ocupation);
+// const people2 = {
+//   name: 'boby',
+//   age: 29,
+//   ocupation: 'dev',
+// }
+
+// console.log(Object.entries(people));
+// const entry = Object.entries(people);
+// const entry = Object.keys(people);
+// const entry = Object.values(people);
+// console.log(entry);
+
+// const mapEntry = entry.map(function(item) {
+//   return item[0];
+// })
+// console.log(mapEntry);
+
+// entry.forEach()
+
+// for (let keys of entry) {
+//   if (keys === 'name') {
+//     console.log(keys);
+//   }
+// }
+
+// const people = {
+//   john: {
+//     name: 'john',
+//     age: 34,
+//     ocupation: 'developer',
+//   },
+//   susan: {
+//     name: 'susan',
+//     age: 24,
+//     ocupation: 'designer',
+//   },
+//   bob: {
+//     name: 'bob',
+//     age: 22,
+//     ocupation: 'salesman',
+//   },
+// }
+
 // greet.call(people.john);
 
-// greet.call(john);
-// greet.call(susan);
+// const test = Object.entries(people).filter(item => {
+//   if (item.includes('john')) {
+//     return item;
+//   }
+// })
+
+// !continue tutorial
+
+// const john = {
+//   name: "john",
+//   age: 24,
+// };
+// const susan = {
+//   name: "susan",
+//   age: 25,
+// };
+
+// function greet(city, country) {
+//   // console.log(this);
+//   console.log(`hello, my name is ${this.name} and im ${this.age} years old
+//   and i live in ${city}, ${country}`);
+// }
+
+// *call
+// greet.call(john, 'san diego', 'us');
+// greet.call(susan, 'san diego', 'us');
+// greet.call({ name: 'peter', age: 30 }, 'san diego', 'us')
+
+// *apply
+// greet.apply(john, ['san diego', 'us']);
+// greet.apply(susan, ['san diego', 'us']);
+// greet.apply({ name: 'peter', age: 30}, ['san diego', 'us'])
+
+// *bind
+// const susanGreet = greet.bind(susan, 'toronto', 'ca');
+// susanGreet();
+
+// !button example
+
+const counter = {
+  count: 0,
+  increment(){
+    // console.log(this);
+    this.count++;
+    console.log(this.count);
+  }
+}
+const btn = document.querySelector('.increment');
+btn.addEventListener("click", counter.increment)
+
+// const inc = counter.increment.bind();
+
